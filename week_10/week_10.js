cb3f044f1c4dfe841713a7d0deac50bdfb0b1b68
Sisolis Wayne <mohammedakoredest@gmail.com>
2023-02-27 13:55:56 +0100

    90% done with day 10

--- week_10/week_10.js
+++ week_10/week_10.js
@@ -1,36 +1,181 @@
-console.log('me')
+        // level 1
+        // 1
+const newSet = new Set()
+console.log(newSet)
 
-const companies = new Set()
-console.log(companies)
-console.log(companies.size)
-
-const languages = [
-    'English',
-    'Finnish',
-    'English',
-    'French',
-    'Spanish',
-    'English',
-    'French',
-    'Finnish'
-]
-
-const setOfLanguages = new Set(languages)
-console.log(setOfLanguages)
-
-for (const language of setOfLanguages) {
-    console.log(language)
+        // 2
+for (let i = 0; i <= 10; i++) {
+    // console.log(i)
+    newSet.add(i)
 }
 
+console.log(newSet)
 
+        // 3
+newSet.delete(4)
+console.log(newSet)
 
-let a = [1, 2, 3, 4, 5]
-let b = [3, 4, 5, 6]
+        // 4
+newSet.clear()
+console.log(newSet)
+
+        // 5
+const newCountries = ['Afghanistan', 'Djibouti', 'Maldives', 'Cuba', 'United Kingdom']
+const newSet2 = new Set()
+
+for (const country of newCountries) {
+    newSet2.add(country)
+}
+
+console.log(newSet2)
+
+        // 6
+const countries = ['Finland', 'Sweden', 'Norway']
+const newMap = new Map()
+// newMap.set('Afghanistan', 'Afghanistan'.length)
+
+for (const country of countries) {
+        newMap.set(country, country.length)
+}
+
+console.log(newMap)
+
+        // level 2
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+
+        // 1
 let c = [...a, ...b]
-
-let C = new Set(c)
-
+const C = new Set(c) // union of a and b
 console.log(C)
+
+        // 2
+const A = new Set(a)
+const B = new Set(b)
+let d = a.filter((item) => B.has(item))
+const D = new Set(d) // intersection of a and b
+// console.log(d)
+console.log(D)
+
+        // 3
+let e = a.filter((item) => !B.has(item)) // difference between a and b i.e a - b
+const E = new Set(e)
+console.log(E)
+
+        // level 3
+import { Countries } from "../week_9/countries_data.js"
+console.log(Countries)
+
+        // 1
+// let nameOfCountries = []
+// for (const country of Countries) {
+//         nameOfCountries.push(country.languages)
+// }  another method as below with 'map'
+
+const languages = Countries.map((item) => item.languages).reduce((acc, curr) => acc.concat(curr), [])
+console.log(languages)
+
+const newSet3 = new Set(languages)
+console.log(newSet3)
+
+        // 2
+// let mostSpokenLanguages = []
+
+// for (const lang of newSet3) {
+//         const filteredLang = languages.filter((item) => item === lang)
+//         // console.log(filteredLang)
+//         mostSpokenLanguages.push({[lang]: filteredLang.length})
+// }
+
+// console.log(mostSpokenLanguages)
+
+const mostSpokenLanguages = (arr, index) => {
+        const languages = arr.map((item) => item.languages).reduce((acc, curr) => acc.concat(curr), [])
+        // console.log(languages)
+        const newSet3 = new Set(languages)
+        let langArr = []
+
+        for (const lang of newSet3) {
+                const filteredLang = languages.filter((item) => item === lang)
+                // console.log(filteredLang)
+                langArr.push({ [lang]: filteredLang.length })
+        }
+
+        return langArr
+
+       
+}
+
+// console.log(mostSpokenLanguages(Countries, 5).Pashto)
+
+
+
+console.log(mostSpokenLanguages(Countries, 5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ week_10/week_10_practice.js
@@ -0,0 +1,45 @@
+console.log('me')
+
+const companies = new Set()
+console.log(companies)
+console.log(companies.size)
+
+const languages = [
+    'English',
+    'Finnish',
+    'English',
+    'French',
+    'Spanish',
+    'English',
+    'French',
+    'Finnish'
+]
+
+const setOfLanguages = new Set(languages)
+console.log(setOfLanguages)
+
+for (const language of setOfLanguages) {
+    console.log(language)
+}
+
+
+
+let a = [1, 2, 3, 4, 5]
+let b = [3, 4, 5, 6]
+let c = [...a, ...b] // spread is another way to concat two arrays
+
+let C = new Set(c)
+
+ console.log(C)
+ console.log(c)
+
+console.log(a.concat(b)) // same result using concat
+
+
+
+
+
+
+
+
+
