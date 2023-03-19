7be5c8ad64478540cec6eb1782566041a8be07c5
Sisolis Wayne <mohammedakoredest@gmail.com>
2023-03-19 18:56:29 +0100

    Done with Day 12😁

--- week_12/week_12.js
+++ week_12/week_12.js
@@ -20,8 +20,7 @@ console.log(sortedPoints)
 const distance = sortedPoints.slice(-1) - sortedPoints[0]
 console.log(distance)
 
-
-
+// 3
 const is_valid_variable = (str) => {
     const pattern = /^[\d+]/g
     const pattern1 = /[-]/g
@@ -34,3 +33,44 @@ console.log(is_valid_variable('first-name'))
 console.log(is_valid_variable('1first_name'))
 console.log(is_valid_variable('first_name'))
 console.log(is_valid_variable('firstname'))
+
+// level 2
+// 1
+paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
+// const arrPgh = paragraph.replace(/[.]/g, '').split(' ')
+// console.log(arrPgh)
+
+const tenMostFrequentWords = (para, index) => {
+    const arr = para.replace(/[%@&;$#!?.]/g, '').split(' ')
+
+    return Object.entries(arr.reduce((acc, curr) => {
+        if (!acc[curr]) {
+            acc[curr] = 1;
+        } else {
+            acc[curr]++;
+        }
+        return acc;
+    }, {})).reduce((acc, curr) => {
+        let obj = {};
+        obj.word = curr[0]
+        obj.count = curr[1]
+        acc.push(obj)
+        return acc;
+    }, []).slice().sort((a, b) => {
+        if (a.count > b.count) return -1
+        if (a.count < b.count) return 1
+        return 0
+    }).slice(0, index)
+}
+
+
+console.log(tenMostFrequentWords(paragraph, 10))
+
+// 3
+const sentence1 = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+
+// const cleanedSentence = sentence1.replace(/[%@&;$#!?]/g, '').split(' ')
+// console.log(cleanedSentence)
+
+// // console.log(cleanText(sentence))
+console.log(tenMostFrequentWords(sentence1))
