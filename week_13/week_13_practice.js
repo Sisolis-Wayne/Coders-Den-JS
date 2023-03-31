3ddd01be2fc3cf54e673fdd0cb02c5b49ec27be4
Sisolis Wayne <mohammedakoredest@gmail.com>
2023-03-31 12:18:30 +0100

    starting Day 13

---
+++ week_13/week_13_practice.js
@@ -0,0 +1,181 @@
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]
+
+console.time('Regular for loop')
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i][0], countries[i][1])
+}
+console.timeEnd('Regular for loop')
+
+console.time('for of loop')
+for (const [name, city] of countries) {
+    console.log(name, city)
+}
+console.timeEnd('for of loop')
+
+console.time('forEach loop')
+countries.forEach(([name, city]) => {
+    console.log(name, city)
+})
+console.timeEnd('forEach loop')
+
+console.info('30 Days Of JavaScript challenge is trending on Github')
+console.info('30 Days Of fullStack challenge might be released')
+console.info('30 Days Of HTML and CSS challenge might be released')
+
+
+console.assert(4 > 3, '4 is greater than 3') // no result
+console.assert(3 > 4, '3 is not greater than 4') // Assertion failed: 3 is not greater than 4
+
+for (let i = 0; i <= 10; i += 1) {
+    let errorMessage = `${i} is not even`
+    console.log('the # is ' + i)
+    console.assert(i % 2 === 0, { number: i, errorMessage: errorMessage })
+}
+
+
+
+
+
+const names = ['Asabeneh', 'Brook', 'David', 'John']
+const countris = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]
+const user = {
+    name: 'Asabeneh',
+    title: 'Programmer',
+    country: 'Finland',
+    city: 'Helsinki',
+    age: 250
+}
+const users = [
+    {
+        name: 'Asabeneh',
+        title: 'Programmer',
+        country: 'Finland',
+        city: 'Helsinki',
+        age: 250
+    },
+    {
+        name: 'Eyob',
+        title: 'Teacher',
+        country: 'Sweden',
+        city: 'London',
+        age: 25
+    },
+    {
+        name: 'Asab',
+        title: 'Instructor',
+        country: 'Norway',
+        city: 'Oslo',
+        age: 22
+    },
+    {
+        name: 'Matias',
+        title: 'Developer',
+        country: 'Denmark',
+        city: 'Copenhagen',
+        age: 28
+    }
+]
+
+console.group('Names')
+console.log(names)
+console.groupEnd()
+
+console.group('Countries')
+console.log(countris)
+console.groupEnd()
+
+console.group('Users')
+console.log(user)
+console.log(users)
+console.groupEnd()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
