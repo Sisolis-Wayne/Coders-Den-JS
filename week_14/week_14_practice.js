2a75dcbd413119881ffe6c6a252cbc6bdda3014b
Sisolis Wayne <mohammedakoredest@gmail.com>
2023-05-07 07:21:28 +0100

    On Day 14. Off to church

---
+++ week_14/week_14_practice.js
@@ -0,0 +1,18 @@
+try {
+    let lastName = 'Yetayeh'
+    let fullName = firstName + ' ' + lastName
+} catch (err) {
+    console.log(err)
+}
+
+try {
+    let lastName = 'Yetayeh'
+    let fullName = firstName + ' ' + lastName
+} catch (err) {
+    // console.error(err) // we can use console.log() or console.error()
+    // console.log(err)
+    throw '404 not found'
+    // err
+} finally {
+    console.log('In any case I will be executed')
+}
